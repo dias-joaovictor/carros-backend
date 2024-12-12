39c515b7333761727e457d46e7a8d4357b85f516
Joao Victor Dias <kvj1610@hotmail.com>
2024-12-12 22:00:54 +0100

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -153,4 +153,5 @@ INSERT INTO usuario (nome, cargo, email, avatar, password) VALUES
                                                                ('Road Runner', 'Testador de Produtos', 'roadrunner@acme.com', 'https://www.gravatar.com/avatar/af5a6b8ed7b0a37e25b1cf5e9e22a8f2?s=200', 'senha123'),
                                                                ('Daffy Duck', 'Gerente de Marketing', 'daffy@acme.com', 'https://www.gravatar.com/avatar/daf4b5b81342fe587d5043b3e58a0180?s=200', 'senha123'),
                                                                ('Bugs Bunny', 'Diretor de TI', 'bugs@acme.com', 'https://www.gravatar.com/avatar/9f0c2cb2d5f8264e2c35d92f3655f2b3?s=200', 'senha123'),
+                                                               ('Bugs Bunny', 'Diretor de TI', 'dias', 'https://www.gravatar.com/avatar/9f0c2cb2d5f8264e2c35d92f3655f2b3?s=200', '123'),
                                                                ('Elmer Fudd', 'Analista de Suporte', 'elmer@acme.com', 'https://www.gravatar.com/avatar/6be1f3c98be3886b5473b7b947fe82ad?s=200', 'senha123');
